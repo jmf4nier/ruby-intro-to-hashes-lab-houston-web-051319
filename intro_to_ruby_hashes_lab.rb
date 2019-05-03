2e3cc9256e28de09f0f599a29a0eb6ac35babd29
Jason Fournier <jmf4nier@hotmail.com>
2019-05-03 20:28:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
 	                :reading_railroad => {:mortgage_value => "$100"},
 	                :pennsylvania_railroad => {:mortgage_value => "$100"}, 
 	                :b_and_o_railroad => {:mortgage_value => "$100"},
-	                :shortline => {}}, 
+	                :shortline => {:mortgage_value => "$100"}}, 
 	              :rent_in_dollars => {
 	                :one_piece_owned => 25,
 	                :two_pieces_owned => 50,
